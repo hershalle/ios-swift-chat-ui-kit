80d086c2e491b2da662c87876c77adb09bed10f4
hershalle <hershalle@gmail.com>
2022-12-02 20:59:06 +0200

    Updates for EmptyMessage

--- Library/UIKit Resources/Helper Extensions/Set Empty Message/EmptyMessage.swift
+++ Library/UIKit Resources/Helper Extensions/Set Empty Message/EmptyMessage.swift
@@ -37,16 +37,34 @@ extension UITableView {
     func setEmptyMessage(_ message: String) {
         let messageLabel = UILabel(frame: CGRect(x: 0, y: 0, width: self.bounds.size.width, height: (self.bounds.size.height/2)))
         messageLabel.text = message
-        if #available(iOS 13.0, *) {
-            messageLabel.textColor = .systemGray
-        } else {
-            messageLabel.textColor = .gray
-        }
+        
+            // SafeUp change:
+//        if #available(iOS 13.0, *) {
+//            messageLabel.textColor = .systemGray
+//        } else {
+//            messageLabel.textColor = .gray
+//        }
+//        messageLabel.numberOfLines = 0;
+//        messageLabel.textAlignment = .center
+//        messageLabel.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+//        messageLabel.sizeToFit()
+//        self.backgroundView = messageLabel
+        // SafeUp change:
+        messageLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.7)
         messageLabel.numberOfLines = 0;
         messageLabel.textAlignment = .center
-        messageLabel.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+        messageLabel.font = UIFont.systemFont(ofSize: 16.0)
         messageLabel.sizeToFit()
-        self.backgroundView = messageLabel
+        
+        let stackView = UIStackView(frame: messageLabel.frame)
+        stackView.addArrangedSubview(messageLabel)
+        stackView.alignment = .bottom
+        stackView.isLayoutMarginsRelativeArrangement = true
+        
+        let inset: CGFloat = 16.0
+        stackView.directionalLayoutMargins = NSDirectionalEdgeInsets(top: inset, leading: inset, bottom: inset, trailing: inset)
+        
+        backgroundView = stackView
     }
     
     func restore() {
